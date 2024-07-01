26ee06eb8375e21beca4c4110670a3cf0de57ac0
jaskaran1234-creator <jaskaransinghsehmi4@gmail.com>
2024-07-01 12:07:10 +0000

    conversion done

---
+++ 01-basics/03conversionoperation.js
@@ -0,0 +1,17 @@
+let score = "33abs"
+
+console.log(typeof score);
+
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+let num=1
+let boo= Boolean(num)
+console.log(typeof boo)
+console.log(boo)
+
+//"33" can be converted into number 33
+//"33abc"--> can not be converted in number
+//true->1, false->0
